af2af67c157db1ebd6111cea979e18ec934c39b5
Tianchen Xu <TianchenX@outlook.com>
2018-09-06 08:47:29 +0800

    Complete direction table down sampling.

--- SurfaceVoxelizerX/CSDownSampleDirTable.hlsl
+++ SurfaceVoxelizerX/CSDownSampleDirTable.hlsl
@@ -2,13 +2,13 @@
 // By XU, Tianchen
 //--------------------------------------------------------------------------------------
 
-#define WRITE_BIT(d, i, s)	d = (((d) & (1 << (i))) | (s))
-#define MERGE(d, i, c, s)	WRITE_BIT(d, i, (c) ? (d) & (s) : (d))
+#define WRITE_BIT(d, m, s)	(((d) & ~(m)) | ((s) & (m)))
+#define MERGE(d, m, c, s)	d = (c) ? WRITE_BIT(d, (m).x | (m).y | (m).z, (d) & (s)) : (d)
 
 RWTexture3D<uint>	g_txGrid;
 RWTexture3D<uint>	g_RWGrid;
 
-groupshared int	g_Block[2][2][2];
+groupshared uint	g_Block[2][2][2];
 
 //--------------------------------------------------------------------------------------
 // Down sampling
@@ -17,24 +17,13 @@ groupshared int	g_Block[2][2][2];
 void main(uint3 DTid : SV_DispatchThreadID, uint3 GTid : SV_GroupThreadID, uint3 Gid : SV_GroupID)
 {
 	uint uData = g_txGrid[DTid];
-	int iData = asint(uData);
-	g_Block[GTid.x][GTid.y][GTid.z] = iData;
+	g_Block[GTid.x][GTid.y][GTid.z] = uData;
 	GroupMemoryBarrierWithGroupSync();
 
-	MERGE(g_Block[GTid.x][GTid.y][GTid.z], 2, GTid.x && GTid.y == 0, g_Block[GTid.x - 1][GTid.y][GTid.z]);
-	MERGE(g_Block[GTid.x][GTid.y][GTid.z], 3, GTid.x && GTid.y == 1, g_Block[GTid.x - 1][GTid.y][GTid.z]);
-	MERGE(g_Block[GTid.x][GTid.y][GTid.z], 4, GTid.x && GTid.z == 0, g_Block[GTid.x - 1][GTid.y][GTid.z]);
-	MERGE(g_Block[GTid.x][GTid.y][GTid.z], 5, GTid.x && GTid.z == 1, g_Block[GTid.x - 1][GTid.y][GTid.z]);
-	/*g_Block[GTid.x][GTid.y][GTid.z] = GTid.x ? g_Block[GTid.x][GTid.y][GTid.z] + g_Block[GTid.x - 1][GTid.y][GTid.z] : g_Block[GTid.x][GTid.y][GTid.z];
-	g_Block[GTid.x][GTid.y][GTid.z] = GTid.y ? g_Block[GTid.x][GTid.y][GTid.z] + g_Block[GTid.x][GTid.y - 1][GTid.z] : g_Block[GTid.x][GTid.y][GTid.z];
-	g_Block[GTid.x][GTid.y][GTid.z] = GTid.z ? g_Block[GTid.x][GTid.y][GTid.z] + g_Block[GTid.x][GTid.y][GTid.z - 1] : g_Block[GTid.x][GTid.y][GTid.z];
-
-	if (all(GTid))
-	{
-		vData = g_Block[GTid.x][GTid.y][GTid.z];
-		g_RWGrid[0][Gid] = vData.x;
-		g_RWGrid[1][Gid] = vData.y;
-		g_RWGrid[2][Gid] = vData.z;
-		g_RWGrid[3][Gid] = vData.w;
-	}*/
+	const uint4 vMask = { 1 << GTid.x, 1 << (2 + GTid.y), 1 << (4 + GTid.z), 1 << 31 };
+	MERGE(g_Block[GTid.x][GTid.y][GTid.z], vMask.yzw, GTid.x, g_Block[GTid.x - 1][GTid.y][GTid.z]);
+	MERGE(g_Block[GTid.x][GTid.y][GTid.z], vMask.zxw, GTid.y, g_Block[GTid.x][GTid.y - 1][GTid.z]);
+	MERGE(g_Block[GTid.x][GTid.y][GTid.z], vMask.xyw, GTid.z, g_Block[GTid.x][GTid.y][GTid.z - 1]);
+	
+	if (all(GTid)) g_RWGrid[Gid] = g_Block[GTid.x][GTid.y][GTid.z];
 }
